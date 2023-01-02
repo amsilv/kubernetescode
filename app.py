d746ebae97419f3d9511c17fa206bcb21f9b1154
amsilv <47160569+amsilv@users.noreply.github.com>
2023-01-02 19:31:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'hello, this is automation using docker, kubernetes, jenkins and ArgoCD!!!!'
+    return 'hello, this is automation using docker, kubernetes, jenkins and ArgoCD + one more execution!!!!'
