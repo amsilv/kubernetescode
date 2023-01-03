293553a6e35732cf54544c20970e2605867f4a1f
amsilv <47160569+amsilv@users.noreply.github.com>
2023-01-03 10:46:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'hello, this is automation using docker, kubernetes, jenkins and ArgoCD + one more execution!!!!'
+    return 'hello, periodo de estudos!!! hoje Deus vai me abençoar com coisas maravilhosas, amém!!!'
