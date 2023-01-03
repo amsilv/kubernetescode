0683561a7a5bd810db640182c18daa744db0721d
amsilv <47160569+amsilv@users.noreply.github.com>
2023-01-03 10:50:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'hello, periodo de estudos!!! hoje Deus vai me abençoar com coisas maravilhosas, amém!!!'
+    return 'hello, periodo de estudos!!! hoje Deus vai me abençoar com coisas maravilhosas, amém!!!!!!'
